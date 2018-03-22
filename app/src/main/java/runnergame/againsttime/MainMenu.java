ff6bab6f15d3cc4737fe5b21e680a5ad959418bd
Jatin Kumar <jatin.kumarroxx@hotmail.com>
2018-03-21 22:18:13 -0400

    Added Project.

--- app/src/main/java/runnergame/againsttime/MainMenu.java
+++ app/src/main/java/runnergame/againsttime/MainMenu.java
@@ -1,5 +1,4 @@
-//Author(s): Jatin Kumar(101035696); victor skierski(100952026); shariq adtani(101031329); Aljon Ramos(101050991)
-//Dis: .
+//Author(s): Jatin Kumar(101035696); victor skierski(100952026); shariq adtani(101031329); Aljon Ramos(101050991).
 package runnergame.againsttime;
 
 import android.support.v7.app.AppCompatActivity;
